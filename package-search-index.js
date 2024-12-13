4a68dab2b6cac9703905d0b48bdb830a92ffceed
S-Vilka <sergei.vilka@metropolia.com>
2024-12-13 18:15:50 +0200

    Initial commit

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"config"},{"l":"controller"},{"l":"eduni.distributions"},{"l":"simu.dao"},{"l":"simu.entity"},{"l":"simu.framework"},{"l":"simu.model"},{"l":"simu.utility"},{"l":"test"},{"l":"view"}];updateSearchResults();
